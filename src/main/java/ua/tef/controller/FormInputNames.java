dcbb0d665db9d65819a3577e66e86c7613cee9f1
Volodymyr <vova.ostapiuk@gmail.com>
2018-04-04 10:46:32 +0300

    Interface with form input names

---
+++ src/main/java/ua/tef/controller/FormInputNames.java
@@ -0,0 +1,17 @@
+package ua.tef.controller;
+
+public interface FormInputNames {
+    String NAME_INPUT_NAME = "name";
+    String SURNAME_INPUT_NAME = "surname";
+    String PATRONYMIC_INPUT_NAME = "patronymic";
+    String NICKNAME_INPUT_NAME = "nickname";
+    String COMMENT_INPUT_NAME = "comment";
+    String HOMEPHONE_INPUT_NAME = "home_phone";
+    String FIRSTMOBILE_INPUT_NAME = "first_mobile_phone_number";
+    String SECONDMOBILE_INPUT_NAME = "second_mobile_phone_number";
+    String INDEX_INPUT_NAME = "index";
+    String CITY_INPUT_NAME = "city";
+    String STREET_INPUT_NAME = "street";
+    String HOUSE_NUMBER_INPUT_NAME = "house_number";
+    String APPARTMENT_NUMBER_INPUT_NAME = "appartment_number";
+}
